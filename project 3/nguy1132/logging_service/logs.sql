ed602b22bf2631c80612fe53537750be7a000e3a
Linh T Nguyen <92663471+linh010604@users.noreply.github.com>
2025-05-26 23:31:07 -0400

    Delete project 3 directory

--- project 3/nguy1132/logging_service/logs.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS logs;
-
-CREATE TABLE logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    event_type TEXT NOT NULL,
-    username TEXT NOT NULL,
-    filename TEXT DEFAULT 'NULL'
-);
